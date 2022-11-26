ece6754d7b9c250b6db0c4d9880e80ae92e99390
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-11-26 22:20:04 +0000

    This program converts celsius to fahrenheit

---
+++ temp_convert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created By: Kent Gatera
+# Date: Nov. 26, 2022
+# This program converts celsius to fahrenheit
+
+
+# Using "def" to define a function.
+def fahrenheit():
+    # The try catch in this case allows decimals because temperature can be a decimal.
+    try:
+        celsius_temp = float(input("What is the temperature? (C): "))
+    except:
+        print("Your input is not a number.")
+
+    # The celsius to fahrenheit formula.
+    fahrenheit_temp = (1.8 * celsius_temp) + 32
+    print("{}(C) = {:.2f}(F)".format(celsius_temp, fahrenheit_temp))
+
+
+def main():
+    # This function calls the fahrenheit function.
+    fahrenheit()
+
+
+# Calling the function that calls the fahrenheit function.
+if __name__ == "__main__":
+    main()
